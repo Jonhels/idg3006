82f8de1efbd9a55aa59458c029cfc80badd65ce3
Jonhels <jonhels8888@gmail.com>
2024-09-28 11:05:34 +0200

    configured server.js, for now its basic setup

---
+++ Backend/config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+dotenv = require("dotenv");
+
+const dbConnect = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `Successfully connected to database: ${connection.connection.host}`,
+    );
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = dbConnect;
