6a115d068104a90c579ef845da370fa4b52a3369
sabrina <sabrina130102@gmail.com>
2024-10-02 12:45:34 +0200

    New brach

---
+++ Frontend/my-app/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+// import Test from './test/Test.js';
+import Yes from './blue/Yes.js'
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <Yes></Yes>
+      <Yes></Yes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/my-app/Test.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Test() {
+  return (
+    <div className="App">
+      <h1>HEllo world</h1>
+    </div>
+  );
+}
+
+export default Test;
