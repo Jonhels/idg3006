e7d9b6ee7c14245d52c68b56c85eea2ff23a48a5
Jonhels <yon@alvinl.com>
2024-10-21 12:12:36 +0000

    added endpoints list in readme, added new endpoints that takes parameters for search based on sensor or event type

--- Backend/routes/mikrobitRoute.js
+++ Backend/routes/mikrobitRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   mikrobitController,
   getAllMikrobits,
+  getFilteredData
 } = require("../controllers/mikrobitController");
 
 const router = express.Router();
 
 router.post("/mikrobit", mikrobitController);
 router.get("/mikrobit", getAllMikrobits);
+router.get("/mikrobit/search", getFilteredData);
 
 module.exports = router;
