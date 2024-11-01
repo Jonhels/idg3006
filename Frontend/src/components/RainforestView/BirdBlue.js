9156afaf519674248de32e7dcaabb9a76efc0eb1
sabrina <sabrina130102@gmail.com>
2024-11-01 16:05:13 +0100

    Deleted Birdblue.js, blad.js, backgroundforest.js

--- Frontend/src/components/RainforestView/BirdBlue.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./rainforest.css";
-// import BirdBlue from "./BirdBlue";
-// import Forestview from "./BackgroundForest";
-// import Bush1 from "./Bush";
-import RainforestSVG from "./RainforestSVG";
