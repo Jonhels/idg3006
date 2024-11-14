bb559bb1d69b9c8d2c96b85c738945c6325e8771
sabrina <sabrina130102@gmail.com>
2024-11-14 18:24:24 +0100

    created animation for RainForest and putted boxText. created antartica file

---
+++ Frontend/src/components/Antartica/Antartica.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AntarticaSVG from "./AntarticaSVG";
+import "./antartica.css";
+
+const Antartica  = () => {
+    return (
+        <div>
+            <AntarticaSVG   />
+        </div>
+
+    );
+}
+
+export default Antartica ;
